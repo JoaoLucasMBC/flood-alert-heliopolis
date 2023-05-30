004fad34fd879fa8e2fde9fa1948427bb894dec8
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-05-30 16:40:49 -0300

    plmds 2

--- back/app.py
+++ back/app.py
@@ -1,15 +1,13 @@
 from flask import Flask, request
 import sqlite3
 from socket import gethostname
-
-import hashlib
-import hmac
-import base64
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 app = Flask(__name__)
 
-TYPEFORM_SECRET_KEY='123'
+
 DATABASE = '/home/eriksoaress/flood-alert-heliopolis-main/back/database/db_alert.db'
 
 
@@ -45,8 +43,8 @@ def post_usuarios():
     if receivedSignature is None:
      return {'error': 'Permission denied.'}, 403
     
-    
-    if(receivedSignature != TYPEFORM_SECRET_KEY):
+    sk = os.getenv("TYPEFORM_SECRET_KEY")
+    if(receivedSignature != sk):
      return {'error': 'Invalid signature. Permission Denied.'}, 403
     
     conn = sqlite3.connect(DATABASE, check_same_thread=False)
