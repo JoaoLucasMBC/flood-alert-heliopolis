c17acde2365f2711f1b529529d3dc72486677468
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-06-02 09:27:52 -0300

    documentação do código

--- back/disparo.py
+++ back/disparo.py
@@ -3,7 +3,21 @@ import os
 
 
 def sendWSP(message, apikey, gid=0):
+    '''
+    Função para enviar mensagens para o grupo de WhatsApp utilizando a API do Whin
+
+    Parâmetros:
+    message: mensagem a ser enviada
+    apikey: chave da API do Whin
+    gid: id do grupo de WhatsApp
+
+    Retorno:
+    resposta da API do Whin
+    '''
+
     url = "https://whin2.p.rapidapi.com/send"
+    
+    # cria o cabeçalho da requisição
     headers = {
 	    "content-type": "application/json",
 	    "X-RapidAPI-Key": apikey,
@@ -11,14 +25,18 @@ def sendWSP(message, apikey, gid=0):
     }
 
     try:
-        if gid==0:
+        if gid==0: # se não for passado o id do grupo, envia para o grupo padrão
             return requests.request("POST", url, json=message, headers=headers)
         else: 
+            # se for passado o id do grupo, envia para o grupo especificado
             url = "https://whin2.p.rapidapi.com/send2group"
             querystring = {"gid":gid}
             return requests.request("POST", url, json=message, headers=headers, params=querystring)
     except requests.ConnectionError:
+        # se der erro de conexão, retorna uma mensagem de erro
         return("Error: Connection Error")
+
+
 
 #################################################################
 # Testing Section
