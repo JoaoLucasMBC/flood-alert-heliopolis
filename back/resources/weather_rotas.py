1b4448dc380adc1803692dcd3c13763753351133
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-06-02 15:00:33 -0300

    rotas weather para acesso como api

---
+++ back/resources/weather_rotas.py
@@ -0,0 +1,50 @@
+from flask_restful import Resource
+
+from disparo import sendWSP
+from checa_clima import verify_risk_today, verify_risk_tomorrow
+
+import os
+
+
+class WeatherToday(Resource):
+
+    def get(self):
+        '''
+        Rota GET para verificar se há risco de enchente hoje
+
+        Return:
+            json: mensagem de que há risco de enchente hoje
+        '''
+        if verify_risk_today():
+            print('tá chovendo pa caraio!')
+            msg2 = {"text":"Mensagem de que tá chovendo muito hoje"}
+            myapikey = os.environ.get('WHIN_API_KEY')
+            mygroup = os.environ.get('GROUP_ID')
+            sendWSP(msg2, myapikey, mygroup)
+
+            return {'message': 'there is risk today'}, 200
+        else:
+            print('não tá chovendo tanto!')
+            return {'message': 'there is no risk today'}, 200
+
+
+class WeatherTomorrow(Resource):
+
+    def get(self):
+        '''
+        Rota GET para verificar se há risco de enchente amanhã
+
+        Return:
+            json: mensagem de que há risco de enchente amanhã
+        '''
+        if verify_risk_tomorrow():
+            print('tá chovendo pa caraio!')
+            msg2 = {"text":"Mensagem de que vai chover amanhã cpa"}
+            myapikey = os.environ.get('WHIN_API_KEY')
+            mygroup = os.environ.get('GROUP_ID')
+            sendWSP(msg2, myapikey, mygroup)
+
+            return {'message': 'there is risk tomorrow'}, 200
+        else:
+            print('não tá chovendo tanto!')
+            return {'message': 'there is no risk tomorrow'}, 200
