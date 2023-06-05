a64d16c11b6f88bc8bcb1bf74a8a97ef3034979c
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-06-05 13:46:43 -0300

    function climatempo done

--- back/api_climatempo.py
+++ back/api_climatempo.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import copy
 
 def get_json_from_api(url):
     response = requests.get(url)
@@ -16,14 +17,19 @@ def verify_risk_climatempo():
     data = json['data']
     today = data[0]['date_br'][:10]
     tomorrow = data[25]['date_br'][:10]
-    for hora in data:
-        if hora['date_br'][:10] == today and hora['rain']['precipitation'] != None:
-            dict_data['today'] += hora['rain']['precipitation']
-        elif hora['date_br'][:10] == tomorrow and  hora['rain']['precipitation'] != None:
-            dict_data['tomorrow'] += hora['rain']['precipitation']
+    list_tomorrow_rain = []
+    try:
+        for hora in data:
+            if hora['date_br'][:10] == today and hora['rain']['precipitation'] != None:
+                dict_data['today'] += hora['rain']['precipitation']
+            elif hora['date_br'][:10] == tomorrow and  hora['rain']['precipitation'] != None:
+                dict_data['tomorrow'] += hora['rain']['precipitation']
+                list_tomorrow_rain += [hora['rain']['precipitation']]
+    except:
+        print("Error")
 
     #adicionar verificação se tem risco
-    if dict_data['tomorrow'] > 30:
+    if (dict_data['tomorrow'] > 30) and not all(x < 15 for x in [sum(list_tomorrow_rain[i:i+4]) for i in range(21)]):
         return True
     return False
 
