4f88716c1b86404a604471efd1f30e60d8626ade
st4pzz <ramellajr@gmail.com>
2023-06-05 11:25:42 -0300

    verify risk tomorrow

--- back/checa_clima.py
+++ back/checa_clima.py
@@ -28,13 +28,13 @@ def create_url(date=datetime.datetime.today().strftime('%Y-%m-%d')):
     return f"https://api.open-meteo.com/v1/forecast?latitude=-23.6045&longitude=-46.5983&hourly=precipitation_probability,precipitation&daily=precipitation_sum&forecast_days=1&start_date={date}&end_date={date}&timezone=America%2FSao_Paulo"
 
 
-def verify_risk_today():
+def verify_risk_today(date=datetime.datetime.today().strftime('%Y-%m-%d')):
     '''
     Função que verifica se há risco de enchente hoje
 
     @return: True se há risco, False se não há
     '''
-    url = create_url() # cria a url
+    url = f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q=-23.606820,-46.596861&dt={date}' # cria a url
     json = get_json_from_api(url) # pega o json da api
 
     try:
@@ -48,23 +48,31 @@ def verify_risk_today():
         return False
 
 
-def verify_risk_tomorrow():
+def verify_risk_tomorrow(date=datetime.datetime.now()):
     '''
     Função que verifica se há risco de chuva forte no dia seguinte segundo a previsão do tempo
 
     @return: True se há risco, False se não há
     '''
-
+    tomorrow_date = date + datetime.timedelta(days=1)
     # pega o json da api
-    url = f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q=-23.606820,-46.596861&days=7'
+    url = f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q=-23.606820,-46.596861&dt={tomorrow_date}'
     json = get_json_from_api(url)
 
+    chuva_moderada = False
+    chuva_forte = False
+
     try:
-        tomorrow = json['forecast']['forecastday'][1]['day']['totalprecip_mm'] # pega a precipitação do dia seguinte
-    
-        if tomorrow > 10: # verifica se a precipitação é maior que 10mm
-            return True #Tem risco
-        return False
+        tomorrow_precip = json['forecast']['forecastday'][1]['day']['totalprecip_mm'] # pega o total da precipitação do dia seguinte
+        tomorrow_condition_text = json['forecast']['forecastday'][1]['day']['condition']['text'] # pega o texto de condição do clima do dia seguinte
+
+        if tomorrow_precip > 10 and tomorrow_precip < 20 or tomorrow_condition_text == "Moderate rain": # verifica se a precipitação esta entre 10mm e 20mm ou o texto de condição indica chuva moderada
+            chuva_moderada = True # Tem risco moderado
+
+        elif tomorrow_precip >= 20 or tomorrow_condition_text == "Heavy rain": # verifica se a precipitação é maior que 20mm ou o texto de condição indica chuva forte
+            chuva_forte = True #Tem risco de chuva forte
+            
+        return chuva_moderada, chuva_forte
     
     except: # se não tiver alguma chave no json por erro da API, retorna False
         return False
