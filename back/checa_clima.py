a6d9e68ddc98e58ccf8037a2e1b32894d9b6302c
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-06-06 16:26:49 -0300

    sem funcao de risco do amanha

--- back/checa_clima.py
+++ back/checa_clima.py
@@ -9,23 +9,15 @@ def get_json_from_api(url):
     @param url: url da api
     @return: json com os dados da api
     '''
-
-    response = requests.get(url) # faz a requisição
-    if response.status_code == 200: # verifica se a requisição foi bem sucedida
-        json_data = response.json() # transforma a resposta em json
-        return json_data
-    else:
+    try:
+        response = requests.get(url) # faz a requisição
+        if response.status_code == 200: # verifica se a requisição foi bem sucedida
+            json_data = response.json() # transforma a resposta em json
+            return json_data
+        else:
+            return None
+    except:
         return None
-    
-
-def create_url(date=datetime.datetime.today().strftime('%Y-%m-%d')):
-    '''
-    Função que cria a url para a api do open-meteo
-
-    @param date: data para a qual se quer a previsão do tempo
-    @return: url da api
-    '''
-    return f"https://api.open-meteo.com/v1/forecast?latitude=-23.6045&longitude=-46.5983&hourly=precipitation_probability,precipitation&daily=precipitation_sum&forecast_days=1&start_date={date}&end_date={date}&timezone=America%2FSao_Paulo"
 
 
 def verify_risk_today(date=datetime.datetime.today().strftime('%Y-%m-%d')):
@@ -41,17 +33,14 @@ def verify_risk_today(date=datetime.datetime.today().strftime('%Y-%m-%d')):
 
     try:
         precipitation = json['forecast']['forecastday'][0]['hour'] # pega a lista de informações sobre o as horas do dia atual
-        sum_precip = 0
-        for hour in precipitation:
-            sum_precip += hour['precip_mm']
-            
-            if str(hour['time'][-5:-3]) == str(datetime.datetime.now().time().hour) :
-                
-                if sum_precip/datetime.datetime.now().time().hour > 7.5 or hour['condition']['text'] == "Heavy rain":
-                    return True
+        hora_atual = datetime.datetime.now().time().hour - 1 # pega a hora atual
+        
+        # pega a precip das duas horas anteriores
+        precip = [precipitation[hora_atual], precipitation[hora_atual-1]]
+        
+        if sum(hour['precip_mm'] for hour in precip)/2 > 7.5 or not all(hour['precip_mm'] < 7.5 for hour in precip) or not all(hour['condition'] ['text'] != "Heavy rain" for hour in precip):
+            return True
 
-        # if sum(precipitation) > 25 or sum(precipitation)/datetime.datetime.now().hour > 4: # verifica se a soma da precipitação é maior que 25mm ou se a média de precipitação por hora é maior que 4mm
-        #     return True #Tem risco
         return False
     
     except: # se não tiver alguma chave no json por erro da API, retorna False
