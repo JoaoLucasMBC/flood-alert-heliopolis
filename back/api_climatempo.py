32fd692d7a7435bec051b25f098b21ffb96b11eb
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-06-06 10:39:15 -0300

    api_climatempo finalizada

--- back/api_climatempo.py
+++ back/api_climatempo.py
@@ -12,24 +12,21 @@ def get_json_from_api(url):
 
 
 def verify_risk_climatempo():
-    dict_data = {"today":0, "tomorrow": 0}
     json = get_json_from_api("http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3679/hours/72?token=b8cbc5c94d430ae14306197f0a396ddc")
     data = json['data']
-    today = data[0]['date_br'][:10]
-    tomorrow = data[25]['date_br'][:10]
-    list_tomorrow_rain = []
+    tomorrow_data = data[24:48]
+    rain_amount = 0
+    list_rain_amount = []
     try:
-        for hora in data:
-            if hora['date_br'][:10] == today and hora['rain']['precipitation'] != None:
-                dict_data['today'] += hora['rain']['precipitation']
-            elif hora['date_br'][:10] == tomorrow and  hora['rain']['precipitation'] != None:
-                dict_data['tomorrow'] += hora['rain']['precipitation']
-                list_tomorrow_rain += [hora['rain']['precipitation']]
+        #adiciona por hora a quantidade de precipitação na precipitação total e na lista que será utilizada em um dos parâmetros 
+        for hora in tomorrow_data:
+            rain_amount += hora['rain']['preciptation']
+            list_rain_amount += [hora['rain']['preciptation']]
     except:
         print("Error")
 
-    #adicionar verificação se tem risco
-    if (dict_data['tomorrow'] > 30) and not all(x < 15 for x in [sum(list_tomorrow_rain[i:i+4]) for i in range(21)]):
+    #caso a precipitação total seja maior que 20 mm ou em alguma hora tenha chovido mais que 7.5mm
+    if (rain_amount > 20) or not all(x < 7.5 for x in list_rain_amount):
         return True
     return False
 
